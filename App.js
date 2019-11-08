1d2b10c0d296a476e2ec985760b70a5c778eddcd
werik.paula <werik.paula@teknisa.com>
2019-11-08 16:07:02 -0300

    request graphql

--- App.js
+++ App.js
@@ -7,14 +7,19 @@
  */
 
 import React from 'react';
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import login from './src/api/login';
 
 export default class App extends React.Component {
-  state = {email: '', password: ''};
+  state = { email: '', password: '', token: null };
+
+  onPressLogin = () => {
+    login(this.state.email, this.state.password)
+  };
 
   render() {
     return (
-      <View style={{paddingTop: 150, flex: 1, backgroundColor: 'white'}}>
+      <View style={{ paddingTop: 150, flex: 1, backgroundColor: 'white' }}>
         <Text
           style={{
             fontSize: 35,
@@ -24,25 +29,25 @@ export default class App extends React.Component {
           OdhenPos
         </Text>
         <TextInput
-          style={{borderColor: 'white', borderWidth: 1, padding: 11}}
+          style={{ borderColor: 'white', borderWidth: 1, padding: 11 }}
           placeholder="Digite seu email"
           value={this.state.email}
           keyboardType="email-address"
           onChangeText={email => {
-            this.setState({email});
+            this.setState({ email });
           }}
         />
         <TextInput
-          style={{borderColor: 'black', borderWidth: 1, padding: 11}}
+          style={{ borderColor: 'black', borderWidth: 1, padding: 11 }}
           placeholder="Digite sua senha"
           value={this.state.password}
           secureTextEntry={true}
           onChangeText={password => {
-            this.setState({password});
+            this.setState({ password });
           }}
         />
-        <View style={{flexDirection: 'row', padding: 2}}>
-          <TouchableOpacity style={{flex: 1}} onPressRegister={() => {}}>
+        <View style={{ flexDirection: 'row', padding: 2 }}>
+          <TouchableOpacity style={{ flex: 1 }} onPress={() => { }}>
             <Text
               style={{
                 color: 'white',
@@ -54,7 +59,7 @@ export default class App extends React.Component {
               Register
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{flex: 1}} onPressLogin={() => {}}>
+          <TouchableOpacity style={{ flex: 1 }} onPress={this.onPressLogin}>
             <Text
               style={{
                 color: 'white',
---
+++ src/api/login.js
@@ -0,0 +1,34 @@
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+    uri: 'http://10.2.2.84:8080/graphql',
+});
+const token = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJoZW5yaXF1ZUBlbWFpbC5jb20iLCJleHAiOjE1NzMzMTY0NDAsImlhdCI6MTU3MzIzMDA0MH0.Tbou8hMVjmCo5uB1YduCbvYrOXUunf4NGLaVhSfRRNllS3fgQtQyFxkJt87W2GZLhYVU0k_WfJ81M_djxnkR0A';
+
+const params = {
+    method: "POST",
+    body: '',
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+};
+
+const login = (email, password) => {
+    return new Promise((resolve, reject) => {
+        client
+            .query({
+                query: gql`
+          {
+            login(email: ${email}, password:${password}) {
+              token
+            }
+          }
+        `
+            })
+            .then(result => resolve(result));
+    });
+}
+
+export default login;
