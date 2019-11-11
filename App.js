c0317076b3f8994f7ce207b95ea50eb1d05c37a8
Bruno Figueiredo <BrunoHRF@outlook.com>
2019-11-11 12:49:26 -0200

    Keyboard Avoid aplicado

--- App.js
+++ App.js
@@ -7,16 +7,23 @@
  */
 
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Alert,
+} from 'react-native';
 import login from './src/api/login';
 
 export default class App extends React.Component {
-  state = { email: '', password: '', token: null };
+  state = {email: '', password: '', token: null};
 
   onPressLogin = () => {
     login(this.state.email, this.state.password).then(result => {
       this.setState({
-        token: result.data.login.token
+        token: result.data.login.token,
       });
       console.log(result.data.login.token);
     });
@@ -24,60 +31,67 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={{ paddingTop: 150, flex: 1, backgroundColor: 'white' }}>
-        <Text
+      <KeyboardAvoidingView style={{flex: 1}}>
+        <View
           style={{
-            fontSize: 35,
-            textAlign: 'center',
-            paddingBottom: 50,
+            flex: 1,
+            backgroundColor: 'white',
+            justifyContent: 'flex-end',
           }}>
-          OdhenPos
-        </Text>
-        <TextInput
-          style={{ borderColor: 'white', borderWidth: 1, padding: 11 }}
-          placeholder="Digite seu email"
-          value={this.state.email}
-          keyboardType="email-address"
-          onChangeText={email => {
-            this.setState({ email });
-          }}
-        />
-        <TextInput
-          style={{ borderColor: 'black', borderWidth: 1, padding: 11 }}
-          placeholder="Digite sua senha"
-          value={this.state.password}
-          secureTextEntry={true}
-          onChangeText={password => {
-            this.setState({ password });
-          }}
-        />
-        <View style={{ flexDirection: 'row', padding: 2 }}>
-          <TouchableOpacity style={{ flex: 1 }} onPress={() => { }}>
-            <Text
-              style={{
-                color: 'white',
-                textAlign: 'center',
-                height: '25%',
-                width: '99%',
-                backgroundColor: '#2196F3',
-              }}>
-              Register
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{ flex: 1 }} onPress={this.onPressLogin}>
-            <Text
-              style={{
-                color: 'white',
-                textAlign: 'center',
-                height: '25%',
-                width: '99%',
-                backgroundColor: '#2196F3',
-              }}>
-              Login
-            </Text>
-          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: 35,
+              textAlign: 'center',
+              paddingBottom: 50,
+            }}>
+            OdhenPos
+          </Text>
+          <TextInput
+            style={{borderColor: 'white', borderWidth: 1, padding: 11}}
+            placeholder="Digite seu email"
+            value={this.state.email}
+            keyboardType="email-address"
+            onChangeText={email => {
+              this.setState({email});
+            }}
+          />
+          <TextInput
+            style={{borderColor: 'black', borderWidth: 1, padding: 11}}
+            placeholder="Digite sua senha"
+            value={this.state.password}
+            secureTextEntry={true}
+            onChangeText={password => {
+              this.setState({password});
+            }}
+          />
+          <View style={{flexDirection: 'row', padding: 2}}>
+            <TouchableOpacity style={{flex: 1}} onPress={() => {}}>
+              <Text
+                style={{
+                  color: 'white',
+                  textAlign: 'center',
+                  height: '25%',
+                  width: '99%',
+                  backgroundColor: '#2196F3',
+                }}>
+                Register
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{flex: 1}} onPress={this.onPressLogin}>
+              <Text
+                style={{
+                  color: 'white',
+                  textAlign: 'center',
+                  height: '25%',
+                  width: '99%',
+                  backgroundColor: '#2196F3',
+                }}>
+                Login
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
